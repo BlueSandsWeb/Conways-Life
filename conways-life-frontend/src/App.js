e4847712f136af37767bbc1b2df6219af0955e49
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-07-16 15:46:42 -0700

    made display grid now clickable to change colors of cells and added event loop

--- conways-life-frontend/src/App.js
+++ conways-life-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import DisplayGrid from './components/DisplayGrid/DisplayGrid'
@@ -7,9 +7,10 @@ import DisplayGrid from './components/DisplayGrid/DisplayGrid'
 function App() {
 
   // ============== Hooks ============== //
-  const [ grid, setGrid ] = useState([])
-  
-  
+  const [grid, setGrid] = useState([])
+  const [changeState, setChangeState] = useState(false) // forces change state, grid is too nested to trigger a change
+  const [stop, setStop] = useState(false) // Check if stop program
+
   // ============== Create empty grid ============== //
   useEffect(() => {
     let testArr = [];
@@ -22,14 +23,56 @@ function App() {
         testArr[i][j] = 0;
       }
     }
-    console.log(testArr)
+    // console.log(testArr)
     setGrid(testArr)
   }, []);
-  
+
+  // ============== Click handler to change individual cells ============== //
+  function changeCell(e) {
+    e.preventDefault()
+    const row = e.target.dataset.row;
+    const col = e.target.dataset.col;
+    let newGrid = grid;
+    newGrid[row][col] == 0 ? newGrid[row][col] = 1 : newGrid[row][col] = 0
+    // console.log("CELL: ", newGrid[row][col])
+    setGrid(newGrid)
+    // console.log(grid)
+    changeState === false ? setChangeState(true) : setChangeState(false)
+  }
+
+  // ============== Algorithm for conway's rules of life ============== //
+  function rulesOfLife() {
+    setStop(false)
+    // rule1: any live cell with fewer than two live neighbors dies, as if by underpopulation.
+    // rule2: any live cell with two or three live neighbors lives on to the next generation.
+    // rule3: any live cell with more than three live neighbors dies, as if by overpopulation
+    // rule4: any dead cell with three live neighbors becomes a live cell, as if by reproduction.
+    let count = 0
+
+    window.setInterval(() => {
+      console.log("Loop")
+      for (let row = 0; row < grid.length; row++) {
+        // column loop
+        for (let col = 0; col < grid[row].length; col++) {
+          let count = 0;
+          // write a check for each individual cell around it
+          console.log("Row: ", row, "Col: ", col)
+          // check stop
+          if (stop) {
+            return console.log("STOPPPPP")
+          }
+        }
+      }
+    }, 1000)
+  }
+
+
   return (
     <div className="App">
       <div className="container">
-        <DisplayGrid grid={grid} setGrid={setGrid} />
+        <DisplayGrid grid={grid} setGrid={setGrid} changeCell={changeCell} />
+        <button onClick={rulesOfLife}>Start!</button>
+        <button onClick={() => setStop(true)}>!!!STOP!!!</button>
       </div>
     </div>
   );
--- conways-life-frontend/src/components/DisplayGrid/DisplayGrid.js
+++ conways-life-frontend/src/components/DisplayGrid/DisplayGrid.js
@@ -1,47 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './DisplayGrid.scss'
 
-function DisplayGrid({grid, setGrid}) {
+function DisplayGrid({ grid, setGrid, changeCell }) {
+
+    // function changeCell(e) {
+    //     e.preventDefault()
+    //     const row = e.target.dataset.row;
+    //     const col = e.target.dataset.col;
+    //     let newGrid = grid;
+    //     newGrid[row][col] == 0 ? newGrid[row][col] = 1 : newGrid[row][col] = 0
+    //     console.log("CELL: ", newGrid[row][col])
+    //     setGrid(newGrid)
+    // }
+
+
 
     return (
         <div className="DisplayGrid--container">
-            {/* loop over grid to make rows */}
-            {/* loop over grid to make columns */}
-                {/* make div cells */}
-                {/* add click handlers to each div cell */}
             {
-                grid.map(row => {
+                grid.map((row, rowIndex) => {
                     return (
-                        <div className="DisplayGrid--row">
-                            {row.map( col => {
-                                console.log("col: ", col)
-                                return (<div className={`DisplayGrid--column ${col === 1 ? "living" : ""}`}></div>);
+                        <div className="DisplayGrid--row" key={`${rowIndex}`}>
+                            {row.map((col, colIndex) => {
+                                return (<div
+                                    onClick={(e) => changeCell(e)}
+                                    className={`DisplayGrid--column ${col === 1 ? "living" : ""}`}
+                                    data-col={`${colIndex}`}
+                                    data-row={`${rowIndex}`}
+                                    key={`${colIndex}${rowIndex}`}>
+                                </div>);
                             })}
                         </div>
                     )
                 })
             }
-
-            {/* <div className="DisplayGrid--row">
-                <div className="DisplayGrid--column living"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-                <div className="DisplayGrid--column"></div>
-            </div> */}
         </div>
     );
 }
+
+
 
 export default DisplayGrid;
